83cb57490e16ab841dceb29fc8e87ea008034f6c
Jonathan Park <joohopark27@gmail.com>
2019-01-15 20:09:09 -0500

    updated robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,20 +7,28 @@
 
 package frc.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+  
+  public static class XBOX {
+    // Buttons
+    public static final int BUTTON_A = 1;
+    public static final int BUTTON_B = 2;
+    public static final int BUTTON_X = 3;
+    public static final int BUTTON_Y = 4;
+    public static final int BUMPER_LEFT = 5;
+    public static final int BUMPER_RIGHT = 6;
+    public static final int BUTTON_BACK = 7;
+    public static final int BUTTON_START = 8;
+    public static final int STICK_LEFT = 9;
+    public static final int STICK_RIGHT = 10;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+    // Axes
+    public static final int STICK_L_X_AXIS = 0;
+    public static final int STICK_L_Y_AXIS = 1;
+    public static final int STICK_R_X_AXIS = 4;
+    public static final int STICK_R_Y_AXIS = 5;
+    public static final int TRIGGER_L_AXIS = 2;
+    public static final int TRIGGER_R_AXIS = 3;
+  }
+  
 }
