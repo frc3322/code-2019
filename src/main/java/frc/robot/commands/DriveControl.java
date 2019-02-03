bbbb52f1f603fce959c3e851895caabd20955e9a
Jonathan Park <joohopark27@gmail.com>
2019-02-03 13:32:47 -0500

    code to have driving sensitive

--- src/main/java/frc/robot/commands/DriveControl.java
+++ src/main/java/frc/robot/commands/DriveControl.java
@@ -21,7 +21,8 @@ public class DriveControl extends Command {
     private final int SPEED_AXIS;
     private final int ROTATION_AXIS;
 
-    private int pow = 1;
+    private int speedPow = 1;
+    private int turnPow = 3;
     private double deadZone = 0.1;
 
     private double maxSpeed = 1;
@@ -41,8 +42,8 @@ public class DriveControl extends Command {
         double speed = Robot.oi.lowerChassis.getRawAxis(SPEED_AXIS);
         double turn = Robot.oi.lowerChassis.getRawAxis(ROTATION_AXIS);
         
-        speed = (Math.abs(speed) > deadZone) ? speed * Math.abs(Math.pow(speed, pow - 1)) : 0;
-        turn = (Math.abs(turn) > deadZone) ? turn * Math.abs(Math.pow(turn, pow - 1)) : 0;
+        speed = (Math.abs(speed) > deadZone) ? speed * Math.abs(Math.pow(speed, speedPow - 1)) : 0;
+        turn = (Math.abs(turn) > deadZone) ? turn * Math.abs(Math.pow(turn, turnPow - 1)) : 0;
 
         if(Math.abs(speed) > maxSpeed) {
             if(speed > 0) {
@@ -52,7 +53,7 @@ public class DriveControl extends Command {
             }
         }
 
-        drivetrain.driveClamped(speed, turn);
+        drivetrain.driveStraight(speed, turn);
         
     }
 
