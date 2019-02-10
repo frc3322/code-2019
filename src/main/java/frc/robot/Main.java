34ca7a112bea0747f44da31c485a0c09566ae951
Jonathan Park <joohopark27@gmail.com>
2019-02-10 15:27:09 -0500

    stuff

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,9 +1,12 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/**
+ *  _____    _____     _____     _____   
+ * |___  \  |___  \   /  _  \   /  _  \
+ *  ___|  |  ___|  | |__| |  | |__| |  |
+ * |___   | |___   |     /  /      /  /
+ *  ___|  |  ___|  |   /  /__    /  /__
+ * |_____/  |_____/   |______|  |______|
+ *
+ */
 
 package frc.robot;
 
