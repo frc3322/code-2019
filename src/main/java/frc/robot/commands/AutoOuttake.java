755f53108debe916a6f162420b45b6714fbd11d7
Jonathan Park <joohopark27@gmail.com>
2019-02-10 14:19:27 -0500

    took back control from first (Thor)

--- src/main/java/frc/robot/commands/AutoOuttake.java
+++ src/main/java/frc/robot/commands/AutoOuttake.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -17,9 +10,6 @@ import static frc.robot.Robot.sideouttake;
 import static frc.robot.Robot.drivetrain;
 import static frc.robot.Robot.oi;
 
-/**
- * Add your docs here.
- */
 public class AutoOuttake extends Command {
 
     public boolean outtaking;
