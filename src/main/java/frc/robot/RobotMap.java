4a68358e28ec588d1e40bbc80c50ee3c2a85d44e
yottalogical <yottalogical@gmail.com>
2019-02-18 10:26:29 -0500

    Add documentation to XBOX controllers

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -71,25 +71,58 @@ public class RobotMap {
     }
   
     public static class XBOX {
-      // Buttons
-        public static final int BUTTON_A = 1;
-        public static final int BUTTON_B = 2;
-        public static final int BUTTON_X = 3;
-        public static final int BUTTON_Y = 4;
-        public static final int BUMPER_LEFT = 5;
-        public static final int BUMPER_RIGHT = 6;
-        public static final int BUTTON_BACK = 7;
-        public static final int BUTTON_START = 8;
-        public static final int STICK_LEFT = 9;
-        public static final int STICK_RIGHT = 10;
+      // Buttons                                       Driver 1:                Driver2:
+        public static final int BUTTON_A = 1;       //                          Lift to bottom level
+        public static final int BUTTON_B = 2;       //                          Lift to middle level
+        public static final int BUTTON_X = 3;       //                          Lift to Cargo Ship level
+        public static final int BUTTON_Y = 4;       //                          Lift to top level
+        public static final int BUMPER_LEFT = 5;    //                          Toggle Inake Pneumatics
+        public static final int BUMPER_RIGHT = 6;   // Toggle hatch gripper     Toggle hatch gripper
+        public static final int BUTTON_BACK = 7;    // Retract Hot Mess         Intake procedure
+        public static final int BUTTON_START = 8;   // Extend Hot Mess          Extend/Retract hatch gripper
+        public static final int STICK_LEFT = 9;     // Gear shift
+        public static final int STICK_RIGHT = 10;   // Auto-align procedure
 
        // Axes
-        public static final int STICK_L_X_AXIS = 0;
-        public static final int STICK_L_Y_AXIS = 1;
-        public static final int STICK_R_X_AXIS = 4;
-        public static final int STICK_R_Y_AXIS = 5;
-        public static final int TRIGGER_L_AXIS = 2;
-        public static final int TRIGGER_R_AXIS = 3;
+        public static final int STICK_L_X_AXIS = 0; // Forward/backwards drive
+        public static final int STICK_L_Y_AXIS = 1; //
+        public static final int STICK_R_X_AXIS = 4; // Left/right drive
+        public static final int STICK_R_Y_AXIS = 5; //
+        public static final int TRIGGER_L_AXIS = 2; // Outtake cargo left       Lift up
+        public static final int TRIGGER_R_AXIS = 3; // Outtake cargo right      Lift down
+        
+        /* Automated Procedures Described:
+          (If any of this seems too difficult or too stupid to implement, talk to either Colt or Fish about it)
+        
+        
+        Auto-Align Procedure:
+          
+          While button is held AND LimeLight can find targets:
+            Robot automatically and continuously aligns based on LimeLight input
+            Driver 1 still has Left/Right control, but their input is mixed with the algorithm’s input
+            Driver 1 still has full control over Forward/Backward, but speed is reduced
+          
+          While button is held AND LimeLight cannot find targets:
+            Driver 1’s controller rumbles
+            Driver 1 has full control over the drivetrain
+            Full speed in maintained
+        
+        
+        Intake Procedure:
+        
+          Once button is pressed:
+            Lift goes to bottom
+            Outtake motors start
+            Intake motors start
+            Intake pneumatics extend
+          
+          Driver 2 can still toggle the intake pneumatics at will
+          
+          Once cargo is detected OR Button is pressed again:
+            Outtake motors stop
+            Intake motors stop
+            Intake pneumatics retract
+        */
   }
   
 }
